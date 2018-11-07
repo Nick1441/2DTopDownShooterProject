d32299a77deaa2086232ee8726b40e32852e7666
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2018-11-07 15:00:19 +0000

    Created Enemy Object
    Created Enemy Object
    - Enemy Walk Towards Player Script
    - Enemy Look At Player Script
    - Enemy Health System

---
+++ TopDownShooterGame2D/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class OnDamagedEvent : UnityEvent<int?> { }
+
+public class Health : MonoBehaviour {
+
+    public int ObjectHealth = 10;
+    public UnityEvent onDie;
+    public OnDamagedEvent onDamaged;
+
+    public void DamageTaken(int damage)
+    {
+        ObjectHealth -= damage;
+        onDamaged.Invoke(ObjectHealth);
+
+        if (ObjectHealth < 1)
+        {
+            onDie.Invoke();
+        }
+    }
+}
