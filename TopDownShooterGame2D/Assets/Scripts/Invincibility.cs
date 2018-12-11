5ce659ea97d2d40db1e11163bc019f2c0c0335ae
Nick1441 <Nick1441@users.noreply.github.com>
2018-12-11 17:33:19 +0000

    Created Pick Up
    Created Pickup Selecter Script.
    Merged Health Pack Script into Pick Up Selecter.
    Created Invincability Script.

---
+++ TopDownShooterGame2D/Assets/Scripts/Invincibility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invincibility : MonoBehaviour {
+
+    public float InvinvabilityTime = 5;
+
+	void Start () {
+        transform.GetComponent<CircleCollider2D>().enabled = false;
+        Invoke("TimeOut", InvinvabilityTime);
+    }
+
+    void TimeOut()
+    {
+        transform.GetComponent<CircleCollider2D>().enabled = true;
+        Destroy(this);
+    }
+}
---
+++ TopDownShooterGame2D/Assets/Scripts/PickUps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickUpSelect
+{
+    Health,
+    invincibility
+}
+
+public class PickUps : MonoBehaviour {
+
+    public PickUpSelect PickupType;
+    public int HealAmount = 10;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (PickupType)
+        {
+            //Sending Health To The Player, Healing Them...
+            case PickUpSelect.Health:
+                other.transform.SendMessage("TakeDamage", -HealAmount, SendMessageOptions.DontRequireReceiver);
+                break;
+
+            case PickUpSelect.invincibility:
+                other.gameObject.AddComponent<invincibility>();
+                break;
+
+            default:
+                break;
+        }
+        Destroy(gameObject);
+    }
+}
